b6f64f396b132031130ce39a0c726df4a0818276
David J. Thomas <dave.a.base@gmail.com>
2016-11-14 19:37:27 -0500

    Corpus downloader added and testing update

--- arakhne/__init__.py
+++ arakhne/__init__.py
@@ -7,6 +7,8 @@ class Arakhne:
         self.title = ''
         self.settings = {}
         self.language = language
+        self.downloader = CorporaDownloader(self.language)
+        self.corpus = None
         # Store all passed kwargs in settings
         if kwargs is not None:
             self.settings.update(kwargs)
@@ -14,3 +16,32 @@ class Arakhne:
         if 'title' in kwargs:
             self.title = kwargs['title']
         self.corpus = Docs(language).corpus
+
+
+class CorporaDownloader:
+
+    def __init__(self, language):
+        self.language = language
+
+    def nltk(self):
+        import nltk
+        nltk.download()
+        return True
+
+    def cltk(self):
+        from cltk.corpus.utils.importer import CorpusImporter
+        try:
+            corpus_importer = CorpusImporter(self.language)
+            corpora_list = corpus_importer.list_corpora
+            if len(corpora_list) == 0:
+                print('Error: No corpora available for download at this time.')
+            else:
+                for corpus in corpora_list:
+                    try:
+                        corpus_importer.import_corpus(corpus)
+                    # ignore unavailable corpora
+                    except:
+                        pass
+        # If there is any problem creating the importer or listing corpora
+        except:
+            print('Error: Unknown error importing corpora.')
--- arakhne/docs/tests/latin_corpus_test.py
+++ arakhne/docs/tests/latin_corpus_test.py
@@ -14,3 +14,39 @@ class LatinCorpusUnitTest(unittest.TestCase):
         ])
         compare = 'Cuius rei'
         return self.assertEqual(test[0], compare)
+
+    # Disabled to improve testing time
+    """
+    def test_macronize(self):
+        LatinCorpus([
+            LatinDoc('Cuius rei'),
+            LatinDoc('verisimilis causa'),
+            LatinDoc('adferebatur, quod Gallis')
+        ]).macronize()
+        return self.assertEqual(True, True)
+    """
+
+    def test_normalize(self):
+        LatinCorpus([
+            LatinDoc('Cuius rei'),
+            LatinDoc('verisimilis causa'),
+            LatinDoc('adferebatur, quod Gallis')
+        ]).normalize()
+        return self.assertEqual(True, True)
+
+    def test_stemmify(self):
+        LatinCorpus([
+            LatinDoc('Cuius rei'),
+            LatinDoc('verisimilis causa'),
+            LatinDoc('adferebatur, quod Gallis')
+        ]).stemmify()
+        return self.assertEqual(True, True)
+
+    def test_clausulae(self):
+        LatinCorpus([
+            LatinDoc('quod superat non est melius quo insumere possis?'),
+            LatinDoc('cur eget indignus quisquam te divite? quare'),
+            LatinDoc('templa ruunt antiqua Deum? cur, inprobe, carae'),
+            LatinDoc('non aliquid patriae tanto emetiris acervo? ')
+        ]).clausulae()
+        return self.assertEqual(True, True)
